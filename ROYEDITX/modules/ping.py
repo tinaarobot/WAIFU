3e6822d35d50ee05e1e2f83d26f5331fe1c69441
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-27 16:34:56 +0530

    Create ping.py

---
+++ ROYEDITX/modules/ping.py
@@ -0,0 +1,19 @@
+import time
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
+
+from datetime import datetime, timedelta
+import time
+from ROYEDITX import application, SUDO_USERS
+
+async def ping(update: Update, context: CallbackContext) -> None:
+    if str(update.effective_user.id) not in sudo_users:
+        update.message.reply_text("✦ ɴᴏ ʙᴀʙʏ, ᴛʜɪs ᴄᴍᴅ ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs..")
+        return
+    start_time = time.time()
+    message = await update.message.reply_text('Pong!')
+    end_time = time.time()
+    elapsed_time = round((end_time - start_time) * 1000, 3)
+    await message.edit_text(f'Pong! {elapsed_time}ms')
+
+application.add_handler(CommandHandler("ping", ping))
