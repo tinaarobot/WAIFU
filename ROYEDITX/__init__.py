c606e83007fa376f16fdf7f765444bd185a99032
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-27 16:15:51 +0530

    Update __init__.py

--- ROYEDITX/__init__.py
+++ ROYEDITX/__init__.py
@@ -3,6 +3,7 @@ import telegram.ext as tg
 from pyrogram import Client
 import logging  
 from telegram.ext import Application
+
 from motor.motor_asyncio import AsyncIOMotorClient
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -15,19 +16,40 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = os.getenv("OWNER_ID")
-sudo_users = os.getenv("SUDO_USERS").split()
-GROUP_ID = os.getenv("GROUP_ID")
-TOKEN = os.getenv("TOKEN")
-mongo_url = os.getenv("MONGO_URL")
-PHOTO_URL = os.getenv("PHOTO_URL").split()
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
-CHARA_CHANNEL_ID = os.getenv("CHARA_CHANNEL_ID")
-API_HASH = os.getenv("API_HASH")
-API_ID = os.getenv("API_ID")
-UPDATE_CHAT = os.getenv("UPDATE_CHAT")
+### ❖ ➥
+OWNER_ID = os.getenv("OWNER_ID", None)
 
-application = Application.builder().token(TOKEN).build()
+### ❖ ➥
+sudo_users = os.getenv("SUDO_USERS", None).split()
+
+### ❖ ➥
+LOGGER_ID = os.getenv("LOGGER_ID", None)
+
+### ❖ ➥
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+
+### ❖ ➥
+MONGO_URL = os.getenv("MONGO_URL", None)
+
+### ❖ ➥
+IMG_URL = os.getenv("IMG_URL", None).split()
+
+### ❖ ➥
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", None)
+
+### ❖ ➥
+CHANNEL_ID = os.getenv("CHANNEL_ID", None)
+
+### ❖ ➥
+API_HASH = os.getenv("API_HASH", None)
+
+### ❖ ➥
+API_ID = os.getenv("API_ID", None)
+
+### ❖ ➥
+UPDATE_CHAT = os.getenv("UPDATE_CHAT", None)
+
+application = Application.builder().token(BOT_TOKEN).build()
 shivuu = Client(
     "lmao",
     api_id=API_ID,
@@ -36,7 +58,7 @@ shivuu = Client(
     
     
 )
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client['Character_catcher']
 collection = db['anime_characters_lol']
 user_totals_collection = db['user_totals_lmaoooo']
