89876036985d0b4559e055b44046ace59db0b244
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-28 13:03:01 +0530

    Update upload.py

--- ROYEDITX/modules/upload.py
+++ ROYEDITX/modules/upload.py
@@ -24,13 +24,13 @@ async def get_next_sequence_number(sequence_name):
 
 async def upload(update: Update, context: CallbackContext) -> None:
     if str(update.effective_user.id) not in SUDO_USERS:
-        await update.message.reply_text('Ask My Owner...')
+        await update.message.reply_text('❖ ᴀsᴋ ᴍʏ ᴏᴡɴᴇʀ...')
         return
 
     try:
         args = context.args
         if len(args) != 4:
-            await update.message.reply_text('✦ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ - /upload ɪᴍɢ_ᴜʀʟ, ᴄʜᴀʀᴀᴄᴛᴇʀ-ɴᴀᴍᴇ, ᴀɴɪᴍᴇ-ɴᴀᴍᴇ, ʀᴀʀɪᴛʏ')
+            await update.message.reply_text('❖ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ /upload ɪᴍɢ_ᴜʀʟ, ᴄʜᴀʀᴀᴄᴛᴇʀ-ɴᴀᴍᴇ, ᴀɴɪᴍᴇ-ɴᴀᴍᴇ, ʀᴀʀɪᴛʏ')
             return
 
         character_name = args[1].replace('-', ' ').title()
@@ -39,14 +39,14 @@ async def upload(update: Update, context: CallbackContext) -> None:
         try:
             urllib.request.urlopen(args[0])
         except:
-            await update.message.reply_text('✦ ɪɴᴠᴀʟɪᴅ ᴜʀʟ...')
+            await update.message.reply_text('❖ ɪɴᴠᴀʟɪᴅ ᴜʀʟ...')
             return
 
-        rarity_map = {1: "⚪ ᴄᴏᴍᴍᴏɴ, 2: "🟣 ʀᴀʀᴇ", 3: "🟡 ʟᴇɢᴇɴᴅᴀʀʏ", 4: "🟢 ᴍᴇᴅɪᴜᴍ"}
+        rarity_map = {1: "ᴄᴏᴍᴍᴏɴ ❤️, 2: "ʀᴀʀᴇ 💛", 3: "ʟᴇɢᴇɴᴅᴀʀʏ 💙", 4: "ᴍᴇᴅɪᴜᴍ 💚"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
-            await update.message.reply_text('✦ ɪɴᴠᴀʟɪᴅ ʀᴀʀɪᴛʏ, ᴘʟᴇᴀsᴇ ᴜsᴇ - 1, 2, 3, ᴏʀ 4')
+            await update.message.reply_text('❖ ɪɴᴠᴀʟɪᴅ ʀᴀʀɪᴛʏ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ 1, 2, 3, ᴏʀ 4')
             return
 
         id = str(await get_next_sequence_number('character_id')).zfill(2)
@@ -62,7 +62,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
         message = await context.bot.send_photo(
             chat_id=CHANNEL_ID,
             photo=args[0],
-            caption=f'<b>✦ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴀᴍᴇ -</b> {character_name}\n<b>✦ ᴀɴɪᴍᴇ ɴᴀᴍᴇ -</b> {anime}\n<b>✦ ʀᴀʀɪᴛʏ -</b> {rarity}\n<b>✦ ɪᴅ -</b> {id}\n✦ ᴀᴅᴅᴇᴅ ʙʏ - <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            caption=f'<b>❖ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴀᴍᴇ ➥</b> {character_name}\n\n<b>● ᴀɴɪᴍᴇ ɴᴀᴍᴇ ➥</b> {anime}\n<b>● ʀᴀʀɪᴛʏ ➥</b> {rarity}\n<b>● ɪᴅ ➥</b> {id}\n\n❖ ᴀᴅᴅᴇᴅ ʙʏ ➥ <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
             parse_mode='HTML'
         )
 
@@ -70,19 +70,19 @@ async def upload(update: Update, context: CallbackContext) -> None:
         await collection.insert_one(character)
 
 
-        await update.message.reply_text('✦ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴀᴅᴅᴇᴅ....')
+        await update.message.reply_text('❖ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴀᴅᴅᴇᴅ....')
     except Exception as e:
-        await update.message.reply_text(f'✦ ᴜɴsᴜᴄᴄᴇssғᴜʟʟʏ ᴜᴘʟᴏᴀᴅᴇᴅ, ᴇʀʀᴏʀ : {str(e)}')
+        await update.message.reply_text(f'❖ ᴜɴsᴜᴄᴄᴇssғᴜʟʟʏ ᴜᴘʟᴏᴀᴅᴇᴅ, ᴇʀʀᴏʀ ➥ {str(e)}')
 
 async def delete(update: Update, context: CallbackContext) -> None:
     if str(update.effective_user.id) not in SUDO_USERS:
-        await update.message.reply_text('✦ ᴀsᴋ ᴍʏ ᴏᴡɴᴇʀ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅs.')
+        await update.message.reply_text('❖ ᴀsᴋ ᴍʏ ᴏᴡɴᴇʀ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅs.')
         return
 
     try:
         args = context.args
         if len(args) != 1:
-            await update.message.reply_text('✦ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛ, ᴘʟᴇᴀsᴇ ᴜsᴇ : /delete ɪᴅ')
+            await update.message.reply_text('❖ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ /delete ɪᴅ')
             return
 
         
@@ -93,42 +93,42 @@ async def delete(update: Update, context: CallbackContext) -> None:
             await context.bot.delete_message(chat_id=CHANNEL_ID, message_id=character['message_id'])
             await update.message.reply_text('✦ ᴅᴏɴᴇ...')
         else:
-            await update.message.reply_text('✦ ɴᴏ ᴄʜᴀʀᴀᴄᴛᴇʀ ғᴏᴜɴᴅ ᴡɪᴛʜ ɢɪᴠᴇɴ ɪᴅ.')
+            await update.message.reply_text('❖ ɴᴏ ᴄʜᴀʀᴀᴄᴛᴇʀ ғᴏᴜɴᴅ ᴡɪᴛʜ ɢɪᴠᴇɴ ɪᴅ.')
     except Exception as e:
         await update.message.reply_text(f'{str(e)}')
 
 async def update(update: Update, context: CallbackContext) -> None:
     if str(update.effective_user.id) not in SUDO_USERS:
-        await update.message.reply_text('✦ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ʀɪɢʜᴛs ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅs.')
+        await update.message.reply_text('❖ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ʀɪɢʜᴛs ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅs.')
         return
 
     try:
         args = context.args
         if len(args) != 3:
-            await update.message.reply_text('✦ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ - /update ɪɴ ғɪᴇʟᴅ new_value')
+            await update.message.reply_text('❖ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ /update ɪɴ ғɪᴇʟᴅ new_value')
             return
 
         # Get character by ID
         character = await collection.find_one({'id': args[0]})
         if not character:
-            await update.message.reply_text('✦ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ...')
+            await update.message.reply_text('❖ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴏᴛ ғᴏᴜɴᴅ...')
             return
 
         # Check if field is valid
         valid_fields = ['img_url', 'name', 'anime', 'rarity']
         if args[1] not in valid_fields:
-            await update.message.reply_text(f'✦ ɪɴᴠᴀɪʟᴅ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ ᴏɴᴇ ᴏғ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ - {", ".join(valid_fields)}')
+            await update.message.reply_text(f'❖ ɪɴᴠᴀɪʟᴅ ғᴏʀᴍᴀᴛᴇ, ᴘʟᴇᴀsᴇ ᴜsᴇ ᴏɴᴇ ᴏғ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ ➥ {", ".join(valid_fields)}')
             return
 
         # Update field
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ ᴄᴏᴍᴍᴏɴ", 2: "🟣 ʀᴀʀᴇ", 3: "🟡 ʟᴇɢᴇɴᴅᴀʀʏ", 4: "🟢 ᴍᴇᴅɪᴜᴍ"}
+            rarity_map = {1: "ᴄᴏᴍᴍᴏɴ ❤️", 2: "ʀᴀʀᴇ 💚", 3: "ʟᴇɢᴇɴᴅᴀʀʏ 💙", 4: "ᴍᴇᴅɪᴜᴍ 💛"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
-                await update.message.reply_text('✦ ɪɴᴠᴀʟɪᴅ ʀᴀʀɪᴛʏ, ᴘʟᴇᴀsᴇ ᴜsᴇ - 1, 2, 3, ᴏʀ 4')
+                await update.message.reply_text('❖ ɪɴᴠᴀʟɪᴅ ʀᴀʀɪᴛʏ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ 1, 2, 3, ᴏʀ 4')
                 return
         else:
             new_value = args[2]
@@ -141,7 +141,7 @@ async def update(update: Update, context: CallbackContext) -> None:
             message = await context.bot.send_photo(
                 chat_id=CHANNEL_ID,
                 photo=new_value,
-                caption=f'<b>Character Name:</b> {character["name"]}\n<b>Anime Name:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+                caption=f'<b>❖ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴀᴍᴇ ➥</b> {character["name"]}\n\n<b>● ᴀɴɪᴍᴇ ɴᴀᴍᴇ ➥</b> {character["anime"]}\n<b>● ʀᴀʀɪᴛʏ ➥</b> {character["rarity"]}\n<b>● ɪᴅ ➥</b> {character["id"]}\n\n❖ ᴜᴘᴅᴀᴛᴇᴅ ʙʏ ➥ <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
                 parse_mode='HTML'
             )
             character['message_id'] = message.message_id
@@ -151,13 +151,13 @@ async def update(update: Update, context: CallbackContext) -> None:
             await context.bot.edit_message_caption(
                 chat_id=CHANNEL_ID,
                 message_id=character['message_id'],
-                caption=f'<b>Character Name:</b> {character["name"]}\n<b>Anime Name:</b> {character["anime"]}\n<b>Rarity:</b> {character["rarity"]}\n<b>ID:</b> {character["id"]}\nUpdated by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+                caption=f'<b>❖ ᴄʜᴀʀᴀᴄᴛᴇʀ ɴᴀᴍᴇ ➥</b> {character["name"]}\n\n<b>● ᴀɴɪᴍᴇ ɴᴀᴍᴇ ➥</b> {character["anime"]}\n<b>● ʀᴀʀɪᴛʏ ➥</b> {character["rarity"]}\n<b>● ɪᴅ ➥</b> {character["id"]}\n\n❖ ᴜᴘᴅᴀᴛᴇᴅ ʙʏ ➥ <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
                 parse_mode='HTML'
             )
 
-        await update.message.reply_text('Updated Done in Database.... But sometimes.. It Takes Time to edit Caption in Your Channel..So wait..')
+        await update.message.reply_text('❖ ᴜᴘᴅᴀᴛᴇᴅ ᴅᴏɴᴇ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ, ʙᴜᴛ sᴏᴍᴇᴛɪᴍᴇs ɪᴛ ᴛᴀᴋᴇs ᴛɪᴍᴇ ᴛᴏ ᴇᴅɪᴛ ᴄᴀᴘᴛɪᴏɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ, sᴏ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...')
     except Exception as e:
-        await update.message.reply_text(f'Error: {str(e)}')
+        await update.message.reply_text(f'⬤ ᴇʀʀᴏʀ ➥ {str(e)}')
 
 UPLOAD_HANDLER = CommandHandler('upload', upload, block=False)
 application.add_handler(UPLOAD_HANDLER)
