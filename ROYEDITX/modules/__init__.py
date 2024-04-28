2ee26a34ba74012f5162f1cf1d06f15e2336cc8a
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-28 13:08:55 +0530

    Update __init__.py

--- ROYEDITX/modules/__init__.py
+++ ROYEDITX/modules/__init__.py
@@ -22,7 +22,7 @@ LOGGER = logging.getLogger(__name__)
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+        "✦ ʏᴏᴜ ᴍᴜsᴛ ʜᴀᴠᴇ ᴀ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ ➥ 3.11.9 , ᴍᴜʟᴛɪᴘʟᴇ ғᴇᴀᴛᴜʀᴇs ᴅᴇᴘᴇɴᴅ ᴏɴ ᴛʜɪs ʙᴏᴛ ᴠᴇʀsɪᴏɴ."
     )
     quit(1)
 
@@ -48,7 +48,7 @@ def __list_all_modules():
                 any(mod == module_name for module_name in all_modules)
                 for mod in to_load
             ):
-                LOGGER.error("Invalid loadorder names, Quitting...")
+                LOGGER.error("✦ ɪɴᴠᴀʟɪᴅ ʟᴏᴀᴅᴇʀ ɴᴀᴍᴇs, ǫᴜɪᴛᴛɪɴɢ...")
                 quit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
@@ -58,7 +58,7 @@ def __list_all_modules():
             to_load = all_modules
 
         if NO_LOAD:
-            LOGGER.info("Not loading: {}".format(NO_LOAD))
+            LOGGER.info("♥︎ ɴᴏᴛ ʟᴏᴀᴅɪɴɢ ➥ {}".format(NO_LOAD))
             return [item for item in to_load if item not in NO_LOAD]
 
         return to_load
@@ -67,6 +67,6 @@ def __list_all_modules():
 
 
 ALL_MODULES = __list_all_modules()
-LOGGER.info("Modules to load: %s", str(ALL_MODULES))
+LOGGER.info("♥︎ ᴍᴏᴅᴜʟᴇs ᴛᴏ ʟᴏᴀᴅ ➥ %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
               
